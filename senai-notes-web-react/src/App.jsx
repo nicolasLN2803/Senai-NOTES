22d22897c2e1e95bc40757a63b23c7a68d0b691a
Kaue <kauebarbosa235@gmail.com>
2025-05-13 21:07:53 -0300

    login

--- senai-notes-web-react/src/App.jsx
+++ senai-notes-web-react/src/App.jsx
@@ -1,11 +1,12 @@
 import Login from "./pages/login"
+import NewUser from "./pages/new-user"
 
 function App() {
 return (
 
   <>
 
-    <Login/>
+    <NewUser/>
 
   </>
 )
--- senai-notes-web-react/src/pages/new-user/index.jsx
+++ senai-notes-web-react/src/pages/new-user/index.jsx
@@ -1,20 +1,45 @@
-import "./new-user.css";  
-                // 🔗 Conecta o CSS desta página
+import "./new-user.css"
+import logo from "../../assets/imgs/logo.svg"
 
-function NewUser () {
-return (
-
-<h1>
-
-FRONT-DOMINA
-
-</h1>
+function NewUser() {
 
 
+    
+        return (
+
+            <>
+                <header></header>
+
+                <main className="page-container">
+
+                    <div className="login-container">
+                        <img className="logo" src={logo} alt=""></img>
+
+                            <h1
+                                className="titulo"> Create Your Account </h1>
+
+                                <p className="subtitulo"> Please log in to continue</p>
+
+                            <p className="textEmail">Email Address</p>
+                            <input className="inpt"  placeholder="email@example.com" />
+
+                            <p className="textPassword">Password</p>
+                            <input className="inpt"  placeholder="password" />
+
+                            <button className="btm" >Login </button>
 
 
-)
+                        
+                    </div>
+
+                </main>
 
 
-}
+            </>
+        )
+    }
+
+
+
 export default NewUser
+
