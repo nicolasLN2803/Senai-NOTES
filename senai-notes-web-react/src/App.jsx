89716bc2406872cac956bcf9052d7fb4eba94a0f
nicolasLN2803 <nicolaslameiras@gmail.com>
2025-05-13 21:54:43 -0300

    finalizacao do HTML e do CSS

--- senai-notes-web-react/src/App.jsx
+++ senai-notes-web-react/src/App.jsx
@@ -1,11 +1,43 @@
 import Login from "./pages/login"
+import NewUser from "./pages/new-user"
+import {BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+
+const isAuthenticated = () =>{
+
+let token = localStorage.getItem("meuToken");
+
+if (token == null) {
+
+return false;
+
+} else {
+
+return true;
+
+}
+
+}
+
 return (
 
   <>
+    <BrowserRouter>
 
-    <Login/>
+      <Routes>
+
+        <Route path="/" element= {<Login/>}></Route>
+        <Route path="/login" element= {<Login/>}></Route>
+        <Route path="/new-user" element= {<NewUser/>}></Route>
+        <Route path="/senai-notes" element= {isAuthenticated() == true? <Notes/> : <Login/>}></Route>
+        <Route path="*" element= {<h1> NOT FOUND </h1>}></Route>
+
+
+      </Routes>
+
+    </BrowserRouter>
+    
 
   </>
 )
--- senai-notes-web-react/src/pages/new-user/index.jsx
+++ senai-notes-web-react/src/pages/new-user/index.jsx
@@ -1,14 +1,116 @@
-import "./new-user.css";  
-                // 🔗 Conecta o CSS desta página
+import "./new-user.css"
+import logo from "../../assets/imgs/logo.svg"
+import bar from "../../assets/imgs/Divider.svg"
+import { useState } from "react";
 
 function NewUser () {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onUserCLick = async () => {
+    
+    if (email == "") {
+
+      alert("Preencha o espaço do email.");
+      return;
+
+    }
+
+    if (password == "") {
+
+      alert("Preencha o espaço da senha.")
+      return;
+
+    }
+    
+    let response = await fetch("http://localhost:3000/users", {
+
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({
+
+
+        email: email,
+        password: password
+
+        })
+    });
+
+    console.log(response);
+
+    if (response.ok == true) {
+
+      alert("Cadastro realizado com sucesso!");
+      console.log(response)
+
+      let json = await response.json();
+
+      let token = json.accessToken;
+
+      console.log("Token: " + token);
+
+      // LOCAL STORAGE
+      localStorage.setItem("meuToken", token);
+
+      window.location.href = "/login"
+
+      } else {
+
+      if (response.status == 401) {
+
+        alert("Credenciais Incorretas. Tente novamente.");
+      } else {
+
+        alert("Erro inesperado. Caso persista, contate os administradores.")
+      }
+
+    }
+
+  }
+
 return (
 
-<h1>
+ <>
+                <header></header>
 
-FRONT-DOMINA
+                <main className="page-container">
 
-</h1>
+                    <div className="login-container">
+                        <div className="borda-interna">
+                        <img className="logo" src={logo} alt=""></img>
+
+                            <h1
+                                id="meutitulo"
+                                className="titulo"> Create Your Account </h1>
+
+                                <p className="subtitulo"> Sing up to start organizing your notes and boost your productivity.</p>
+
+                            <p className="textEmail">Email Adress</p>
+                            <input className="inpt" value={email} onChange={event => setEmail(event.target.value)} type="email" placeholder="email@example.com" />
+
+                            <p className="textPassword">Password</p>
+                            <input className="inpt"  value={password} onChange={event => setPassword(event.target.value)} type="password" placeholder="password" />
+                            <p className="senhaSubtitulo"> At least 8 characters</p>
+                            
+                            <button className="btm" onClick={onUserCLick}> Sign up </button>
+
+                            <img className="barra" src={bar} alt=""></img>
+
+                            <a className="form-hint" href="/login"> Already have an account? Login </a>
+
+                            </div>
+
+
+                        
+                    </div>
+
+                </main>
+
+
+            </>
 
 
 
